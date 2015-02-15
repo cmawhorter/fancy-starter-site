c7d31a781f3cd62ee5d6b9dacc974e4cfd26671e
cmawhorter <cory.mawhorter@gmail.com>
2015-02-14 19:05:26 -0800

    wip uses

--- data/providers/products/index.js
+++ data/providers/products/index.js
@@ -2,7 +2,7 @@ module.exports = function(provider) {
   for (var i=0; i < 3; i++) {
     provider.create(i, {
       global: [
-        [ 'resource',   'test' ],
+        [ 'resource',   'someResource' ],
         [ 'title',      'Provider' + i ],
         [ 'route',      '/provider-' + i ],
         [ 'body',       new Date().toISOString() ],
